188386f3d457c2b29115621bf08c505ccd9c7e79
Carlos David <carlosmontellano@gmail.com>
2018-06-10 21:28:22 -0400

    prueba dos carga adecuadamene de oraciones con otro enfoque

--- src/test/java/AnalyzerTest.java
+++ src/test/java/AnalyzerTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,12 +8,13 @@ import static org.junit.Assert.*;
 
 
 public class AnalyzerTest {
+
     @Test public void analyzerReadFileMethodFileNotCanOpen() {
         Analyzer classAnalyzerTest = new Analyzer();
         List<Sentence> listaesperada = new ArrayList();
         assertEquals(listaesperada, classAnalyzerTest.readFile("d:\\prueba.txt"));
     }
-    @Test public void analyzerReadFileMethodFileWithSentencesValid() {
+    @Test public void analyzerReadFileMethodFileWithSentencesValid() throws IOException {
         Analyzer classAnalyzerTest = new Analyzer();
         List<Sentence> listaesperada = new ArrayList();
         listaesperada.add(new Sentence(0, "This was not as much fun as I thought it would be ."));
@@ -21,15 +23,28 @@ public class AnalyzerTest {
         listaesperada.add(new Sentence(2, "I didn’t think programming in Java could be so much fun !"));
         listaesperada.add(new Sentence(-2, "I would have preferred an easier assignment ."));
         listaesperada.add(new Sentence(2, "I can’t think of anything more fun than learning Java !"));
-
-        assertEquals(listaesperada, classAnalyzerTest.readFile("d:\\\\maestria\\\\UnitTestingProjectUASB\\\\files\\prueba1.txt"));
+        String filename=FileName (listaesperada,"prueba1.txt");
+        assertEquals(listaesperada, classAnalyzerTest.readFile(filename));
+    }
+    private String FileName (List<Sentence> lista,String File) throws IOException {
+        File miDir = new File(".");
+        String archivo="";
+        try {
+            archivo=miDir.getCanonicalPath()+"\\files\\"+File;
+        }
+        catch(Exception e) {
+            //e.printStackTrace();
+        }
+        try ( BufferedWriter bw =
+                      new BufferedWriter (new FileWriter (archivo)) )
+        {
+            for(Sentence sentence : lista) {
+                bw.write(sentence.getScore()+" "+sentence.getText()+"\n");
+            }
+            bw.close ();
+        } catch (IOException e) {
+            e.printStackTrace ();
+        }
+        return archivo;
     }
 }
-/*
-0 This was not as much fun as I thought it would be .
-1 I had a lot of fun on this and learned a lot .
--1 It would be more fun if we had more time to work on it .
-2 I didn’t think programming in Java could be so much fun !
-        -2 I would have preferred an easier assignment .
-        2 I can’t think of anything more fun than learning Java !
-        */
